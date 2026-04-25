4bd68605d7155dba39744b680c4b60622a1ac887
Bryan Mata <bryanmata314@gmail.com>
2026-04-24 23:39:18 -0500

    fix(ci): explicitly set dev profile for tests

--- backend/src/test/java/edu/tcu/cs/projectpulse/ProjectpulseApplicationTests.java
+++ backend/src/test/java/edu/tcu/cs/projectpulse/ProjectpulseApplicationTests.java
@@ -2,8 +2,10 @@ package edu.tcu.cs.projectpulse;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("dev")
 class ProjectpulseApplicationTests {
 
     @Test
